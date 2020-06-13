e07f2127b873559219989e9ad4e5b7dba9005ab7
CintiaLimon <66183538+CintiaLimon@users.noreply.github.com>
2020-06-12 22:33:15 -0500

    Create Reto1_3.js

---
+++ Reto1_3.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(255);
+  stroke(255,0,0);
+  strokeWeight(25);
+  noFill();
+  line(115,130,300,275);
+  ellipse(200,200,250);
+  
+}
